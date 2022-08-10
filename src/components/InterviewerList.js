e03b0f6265f95dee3673af924963f1642f595f72
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-09 20:22:52 -0600

    buildup and refactor InterviewerList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import InterviewerListItem from 'components/InterviewerListItem';
+import "components/InterviewerList.scss";
+
+//the function of the InterviewerList component
+//Object.values(props.interviewers);
+export default function InterviewerList(props) {
+
+const intArr = props.interviewers;
+
+  //the array of InterviewerList's child InterviewerListItem component
+  const interviewersArr = intArr.map(_interviewer => {
+    return (
+      <InterviewerListItem
+        key={_interviewer.id}
+        name={_interviewer.name} 
+        avatar={_interviewer.avatar} 
+        selected={_interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(_interviewer.id)}  
+      />
+    );
+  });
+  
+  //the rendering of the InterviewerList component
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewersArr}
+      </ul>
+    </section>
+  
+  );
+}
